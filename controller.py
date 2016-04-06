4b84eef669f8c63a5a49710e9de13614b49bdc18
brentzucker <brentzucker@gmail.com>
2016-04-06 11:46:19 -0400

    Controller works for all programs

--- controller.py
+++ controller.py
@@ -25,46 +25,45 @@ input_sizes = [
 TRIALS = 10
 MAX_NUM = 100000000
 
-p = 'HeapSort'
-# for p in programs:
+for p in programs:
 
-# Reset output file
-command = '> out/{}.out'.format(p)
-os.system(command)
+	# Reset output file
+	command = '> out/{}.out'.format(p)
+	os.system(command)
 
-for size in input_sizes:
-	for trial in range(TRIALS):
-		
-		# Create and open input file
-		f = '{}-{}.{}'.format(p, size, trial)
-		input_file = 'in/{}.in'.format(f)
-		fo = open(input_file, 'w+')
+	for size in input_sizes:
+		for trial in range(TRIALS):
+			
+			# Create and open input file
+			f = '{}-{}.{}'.format(p, size, trial)
+			input_file = 'in/{}.in'.format(f)
+			fo = open(input_file, 'w+')
 
-		# Generate Data Set
-		for i in range(size):
-			num = randint(0, MAX_NUM)
-			# print num
-			fo.write(str(num))
-			fo.write(' ') # delimeter
+			# Generate Data Set
+			for i in range(size):
+				num = randint(0, MAX_NUM)
+				# print num
+				fo.write(str(num))
+				fo.write(' ') # delimeter
 
-		# Prepare to Run Java Program
-		command = 'java {} {}'.format(p, input_file)
-		
-		# Start Timer
-		start = time.time()
+			# Prepare to Run Java Program
+			command = 'java {} {}'.format(p, input_file)
+			
+			# Start Timer
+			start = time.time()
 
-		# Run Java Program
-		os.system(command)
+			# Run Java Program
+			os.system(command)
 
-		# End Timer
-		end = time.time()
+			# End Timer
+			end = time.time()
 
-		time_elapsed = end - start
+			time_elapsed = end - start
 
-		# Save time elapsed
-		command = 'echo "{} {}" >> out/{}.out'.format(f, time_elapsed, p)
-		os.system(command)
-print 'done'
+			# Save time elapsed
+			command = 'echo "{} {}" >> out/{}.out'.format(f, time_elapsed, p)
+			os.system(command)
+	print 'done'
 
 command = 'echo "Complete!" | mail brentzucker@gmail.com'
 os.system(command)
